2771742a9f98f2226eda10eb3aebf37fc48d0857
Daniel Adetaiwo <danieladetaiwoeniola@gmail.com>
2026-01-17 02:33:01 +0100

    icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './globals.css'
 import {Lato} from "next/font/google"
 import Header from './components/header';
-
+import type {Metadata} from 'next'
 const lato = Lato({subsets:["latin"],weight:["100","300","400","700","900"]})
 
-
+export const metadata:Metadata = {
+  title:"NACOS LASU",
+  icons:'/logo.png'
+}
 function RootLayout({ children }: { children: React.ReactNode }) {
 
   return (
